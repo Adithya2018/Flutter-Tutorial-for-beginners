212b2fa506df4b1fc8c01a1fe1ab4492ec2f73e7
Adithyan S Raj <adithyansraj20@gmail.com>
2021-01-09 12:22:08 +0530

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,37 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Not my First'),
-      centerTitle: true,
-      backgroundColor: Colors.green[400],
-    ),
-    body: Center(
-      child: Text(
-        'Hello People!!!',
-        style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower',
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('+'),
-      backgroundColor: Colors.red[400],
-
-    ),
-  ),
+  home:
 ));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Not my First'),
+        centerTitle: true,
+        backgroundColor: Colors.green[400],
+      ),
+      body: Center(
+        child: Text(
+          'Hello People!!!',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('+'),
+        backgroundColor: Colors.red[400],
+
+      ),
+    );
   }
 }
 
