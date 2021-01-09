549f31c346ebf76d8d3ac7b197cede5d5ac6bd08
Adithyan S Raj <adithyansraj20@gmail.com>
2021-01-09 15:15:16 +0530

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -15,7 +15,11 @@ class Home extends StatelessWidget {
       ),
       body: Center(
         // child: Image.network('https://images.unsplash.com/photo-1504893524553-b855bce32c67?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80'),
-
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.red[200],
+          size: 250,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
