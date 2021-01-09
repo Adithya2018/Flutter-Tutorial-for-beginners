1aaf19d566cc00072a9ca7aebd08ad25b1e469b0
Adithyan S Raj <adithyansraj20@gmail.com>
2021-01-09 16:43:37 +0530

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -13,36 +13,37 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.green[400],
       ),
-      body: Center(
-        // child: Image.network('https://images.unsplash.com/photo-1504893524553-b855bce32c67?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80'),
-        // child: Icon(
-        //   Icons.airport_shuttle,
-        //   color: Colors.red[200],
-        //   size: 250,
-        // ),
-        // child: FlatButton(
-        //   onPressed: () {
-        //     print('Oh you clicked Hard!');
-        //   },
-        //   child: Text('Press Hard'),
-        //   color: Colors.lightGreen[200],
-        // ),
-        // child: RaisedButton.icon(
-        //   onPressed: () {},
-        //   icon: Icon(
-        //     Icons.mail
-        //   ),
-        //   label: Text('Mail me'),
-        //   color: Colors.pink[200],
-        // ),
-        child: IconButton(
-          onPressed: () {
-            print('mail');
-          },
-          icon: Icon(Icons.alternate_email),
-          color: Colors.pink[200],
-        ),
-      ),
+      // body: Center(
+      //   // child: Image.network('https://images.unsplash.com/photo-1504893524553-b855bce32c67?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80'),
+      //   // child: Icon(
+      //   //   Icons.airport_shuttle,
+      //   //   color: Colors.red[200],
+      //   //   size: 250,
+      //   // ),
+      //   // child: FlatButton(
+      //   //   onPressed: () {
+      //   //     print('Oh you clicked Hard!');
+      //   //   },
+      //   //   child: Text('Press Hard'),
+      //   //   color: Colors.lightGreen[200],
+      //   // ),
+      //   // child: RaisedButton.icon(
+      //   //   onPressed: () {},
+      //   //   icon: Icon(
+      //   //     Icons.mail
+      //   //   ),
+      //   //   label: Text('Mail me'),
+      //   //   color: Colors.pink[200],
+      //   // ),
+      //   // child: IconButton(
+      //   //   onPressed: () {
+      //   //     print('mail');
+      //   //   },
+      //   //   icon: Icon(Icons.alternate_email),
+      //   //   color: Colors.pink[200],
+      //   // ),
+      // ),
+      body: Container(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('Add'),
