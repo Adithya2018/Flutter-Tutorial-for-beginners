16236ca6eab528f88954b7ff7276252a96c09682
Adithyan S Raj <adithyansraj20@gmail.com>
2021-01-09 12:39:10 +0530

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -14,15 +14,8 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.green[400],
       ),
       body: Center(
-        child: Text(
-          'Hello People',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
+        child: Image(
+          image: NetworkImage('https://images.unsplash.com/photo-1502631868851-1717aca1be29?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
