077fc4c6b923716b00ad360040f6ad5cce9feb3f
Adithyan S Raj <adithyansraj20@gmail.com>
2021-01-09 13:49:50 +0530

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -16,7 +16,7 @@ class Home extends StatelessWidget {
       body: Center(
         child: Image(
           // image: NetworkImage('https://images.unsplash.com/photo-1502631868851-1717aca1be29?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'),
-          image: AssetImage('assets/road.jpg'),
+          image: AssetImage('assets/rose.jpg'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
