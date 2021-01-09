1a7142c776267c0a1aa217f7a461fdf7472a0688
Adithyan S Raj <adithyansraj20@gmail.com>
2021-01-09 08:46:40 +0530

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hey Ninjas:'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('Not my First'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('Hello People!!!'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('+'),
+    ),
+  ),
 ));
