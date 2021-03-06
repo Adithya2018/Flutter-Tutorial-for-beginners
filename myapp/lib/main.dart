756e1a7547c33105be5313060d09809cbb09f94a
Adithyan S Raj <adithyansraj20@gmail.com>
2021-03-06 19:18:56 +0530

    #8 Images & Assets

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -15,15 +15,23 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'Hello Guys',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
+        // child: Text(
+        //   'Hello Guys',
+        //   style: TextStyle(
+        //     fontSize: 20.0,
+        //     fontWeight: FontWeight.bold,
+        //     letterSpacing: 2.0,
+        //     color: Colors.grey[600],
+        //     fontFamily: 'IndieFlower',
+        //   ),
+        // ),
+
+        // child: Image(
+        //   image: NetworkImage('https://images.unsplash.com/photo-1614907301762-405163ba662f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'),
+        // ),
+
+        child: Image(
+          image: AssetImage('../assets/swim.jpg'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
