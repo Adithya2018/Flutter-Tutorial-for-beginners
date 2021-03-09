ec743341a788390a52be71602d87418649cfaaf7
Adithya2018 <adithyans441@gmail.com>
2021-03-09 20:23:29 +0530

    #16 Stateful Widgets

--- ninja_id/lib/main.dart
+++ ninja_id/lib/main.dart
@@ -4,7 +4,116 @@ void main() => runApp(MaterialApp(
   home: NinjaCard(),
 ));
 
-class NinjaCard extends StatelessWidget {
+// class NinjaCard extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.grey[900],
+//       appBar: AppBar(
+//         title: Text('Ninja ID Card'),
+//         centerTitle: true,
+//         backgroundColor: Colors.grey[850],
+//         elevation: 0.0,
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: <Widget>[
+//             Center(
+//               child: CircleAvatar(
+//                 backgroundImage: NetworkImage('https://static.wikia.nocookie.net/streetfighter/images/e/e3/Chunrender.png'),
+//                 radius: 60.0,
+//                 // child: Text(
+//                 //     'CL',
+//                 //     style: TextStyle(
+//                 //       fontSize: 30.0,
+//                 //     ),
+//                 // ),
+//               ),
+//             ),
+//
+//             Divider(
+//               height: 100.0,
+//               color: Colors.grey[800],
+//             ),
+//             Text(
+//               'NAME',
+//               style: TextStyle(
+//                 color: Colors.grey,
+//                 letterSpacing: 2.0,
+//               ),
+//             ),
+//             SizedBox(
+//               height: 10.0,
+//             ),
+//             Text(
+//               'Chun-Li',
+//               style: TextStyle(
+//                 color: Colors.amberAccent[200],
+//                 letterSpacing: 2.0,
+//                 fontSize: 28.0,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             SizedBox(
+//               height: 30.0,
+//             ),
+//             Text(
+//               'CURRENT NINJA LEVEL',
+//               style: TextStyle(
+//                 color: Colors.grey,
+//                 letterSpacing: 2.0,
+//               ),
+//             ),
+//             SizedBox(
+//               height: 10.0,
+//             ),
+//             Text(
+//               '8',
+//               style: TextStyle(
+//                 color: Colors.amberAccent[200],
+//                 letterSpacing: 2.0,
+//                 fontSize: 28.0,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             SizedBox(
+//               height: 30.0,
+//             ),
+//             Row(
+//               children: <Widget>[
+//                 Icon(
+//                   Icons.email,
+//                   color: Colors.grey[400],
+//                 ),
+//                 SizedBox(width: 10.0),
+//                 Text(
+//                   'chun.li@netninja.com',
+//                   style: TextStyle(
+//                     color: Colors.grey[400],
+//                     fontSize: 18.0,
+//                     letterSpacing: 1.0,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +124,17 @@ class NinjaCard extends StatelessWidget {
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
+
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
@@ -70,7 +190,7 @@ class NinjaCard extends StatelessWidget {
               height: 10.0,
             ),
             Text(
-              '8',
+              '$ninjaLevel',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 2.0,
@@ -104,3 +224,4 @@ class NinjaCard extends StatelessWidget {
     );
   }
 }
+
